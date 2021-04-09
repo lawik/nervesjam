7dbcc83418e9785cdb5a38bd16db255b1b9aef6f
Lars Wikman <lars@underjord.io>
2021-04-09 11:13:10 +0200

    Fix release config and add build.sh

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -27,7 +27,7 @@ config :nervespub, NervespubWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :nervespub, NervespubWeb.Endpoint, server: true
+config :nervespub, NervespubWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
